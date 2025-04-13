ab95efbd73b02de80f13f20c79604f059465d6f6
zjp <jiping_zhou@foxmail.com>
2025-04-13 17:15:48 +0800

    test: rename contract1 to contract_requires

--- tests/standard_proofs_with_contracts.rs
+++ tests/standard_proofs_with_contracts.rs
@@ -1,10 +1,10 @@
 #[cfg(kani)]
 mod verify {
     #[kani::requires(a > 0)]
-    fn contract1(a: u8) {}
+    fn contract_requires(a: u8) {}
 
     #[kani::proof]
     fn standard_proof_with_contract_requires() {
-        contract1(0);
+        contract_requires(0);
     }
 }
