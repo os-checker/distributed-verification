c52c8d68e0d3311d031018f061dba7b640ac89e6
zjp <jiping_zhou@foxmail.com>
2025-04-13 17:20:47 +0800

    test: add contract_requires
    This test is weird, because it influences contract_requires's callees.
    Need to figure out why contract_requires's callees changes (not only
    DefIds, but also more importantly the callee instances.

--- tests/standard_proofs_with_contracts.rs
+++ tests/standard_proofs_with_contracts.rs
@@ -7,4 +7,14 @@ mod verify {
     fn standard_proof_with_contract_requires() {
         contract_requires(0);
     }
+
+    #[kani::ensures(|&ret| ret > 0)]
+    fn contract_ensures(a: u8) -> u8 {
+        a
+    }
+
+    #[kani::proof]
+    fn standard_proof_with_contract_ensures() {
+        contract_ensures(0);
+    }
 }
