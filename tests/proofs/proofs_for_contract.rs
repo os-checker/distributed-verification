41db961509d7ae11db31bbcf2409403f2829f1c1
zjp <jiping_zhou@foxmail.com>
2025-04-15 17:08:12 +0800

    test: proof_for_contract specifies one, but call another contract inside

--- tests/proofs/proofs_for_contract.rs
+++ tests/proofs/proofs_for_contract.rs
@@ -18,6 +18,12 @@ mod verify {
         contract_ensures(0);
     }
 
+    #[kani::proof_for_contract(contract_requires)]
+    fn two_contracts_requires_and_ensures() {
+        let val = contract_ensures(0);
+        contract_requires(val);
+    }
+
     #[kani::requires(a > 0)]
     #[kani::ensures(|&ret| ret > 0)]
     fn contract(a: u8) -> u8 {
