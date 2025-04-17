3896f1f943abe75b7111616a2b18d74bd6b94ac9
zjp <jiping_zhou@foxmail.com>
2025-04-17 10:17:47 +0800

    test: assert_unique_hash for proofs under compare folder

--- tests/proofs.rs
+++ tests/proofs.rs
@@ -4,9 +4,9 @@ use std::path::{Path, PathBuf};
 mod utils;
 use utils::{assert_eq, *};
 
-pub fn get_proofs() -> Result<Vec<PathBuf>> {
+fn get_proofs(dir: &str) -> Result<Vec<PathBuf>> {
     let mut proofs = vec![];
-    for entry in std::fs::read_dir("tests/proofs")? {
+    for entry in std::fs::read_dir(dir)? {
         let entry = entry?;
         let path = entry.path();
         if path.is_file()
@@ -17,34 +17,6 @@ pub fn get_proofs() -> Result<Vec<PathBuf>> {
     }
     proofs.sort();
     Ok(proofs)
-}
-
-#[test]
-fn test_proofs() -> Result<()> {
-    let proofs = get_proofs()?;
-
-    expect![[r#"
-        [
-            "tests/proofs/ad_hoc.rs",
-            "tests/proofs/gen_by_macros.rs",
-            "tests/proofs/proofs_for_contract.rs",
-            "tests/proofs/standard_proofs.rs",
-            "tests/proofs/standard_proofs_with_contracts.rs",
-        ]
-    "#]]
-    .assert_debug_eq(&proofs);
-
-    let mut v_json = Vec::<Vec<SerFunction>>::with_capacity(proofs.len());
-    for path in &proofs {
-        let file_stem = path.file_stem().and_then(|f| f.to_str()).unwrap();
-        let text = cmd(&[&format!("tests/proofs/{file_stem}.rs")]);
-        expect_file![format!("./snapshots/{file_stem}.json")].assert_eq(&text);
-        v_json.push(serde_json::from_str(&text).unwrap());
-    }
-
-    assert_unique_hash(&proofs, &v_json);
-
-    Ok(())
 }
 
 fn assert_unique_hash(proofs: &[PathBuf], v_json: &[Vec<SerFunction>]) {
@@ -66,6 +38,59 @@ fn assert_unique_hash(proofs: &[PathBuf], v_json: &[Vec<SerFunction>]) {
              {v_item_len} items have the hash value:\n{v_item:?}",
         );
     }
+}
+
+#[test]
+fn test_proofs() -> Result<()> {
+    let proofs = get_proofs("tests/proofs")?;
+
+    expect![[r#"
+        [
+            "tests/proofs/ad_hoc.rs",
+            "tests/proofs/gen_by_macros.rs",
+            "tests/proofs/proofs_for_contract.rs",
+            "tests/proofs/standard_proofs.rs",
+            "tests/proofs/standard_proofs_with_contracts.rs",
+        ]
+    "#]]
+    .assert_debug_eq(&proofs);
+
+    let mut v_json = Vec::<Vec<SerFunction>>::with_capacity(proofs.len());
+    for path in &proofs {
+        let file_stem = path.file_stem().and_then(|f| f.to_str()).unwrap();
+        let text = cmd(&[&format!("tests/proofs/{file_stem}.rs")]);
+        expect_file![format!("./snapshots/{file_stem}.json")].assert_eq(&text);
+        v_json.push(serde_json::from_str(&text).unwrap());
+    }
+
+    assert_unique_hash(&proofs, &v_json);
+    Ok(())
+}
+
+#[test]
+fn test_compare() -> Result<()> {
+    let proofs = get_proofs("tests/compare")?;
+
+    expect![[r#"
+        [
+            "tests/compare/contract1.rs",
+            "tests/compare/contract2.rs",
+            "tests/compare/proof1.rs",
+            "tests/compare/proof2.rs",
+        ]
+    "#]]
+    .assert_debug_eq(&proofs);
+
+    let mut v_json = Vec::<Vec<SerFunction>>::with_capacity(proofs.len());
+    for path in &proofs {
+        let file_stem = path.file_stem().and_then(|f| f.to_str()).unwrap();
+        let text = cmd(&[&format!("tests/compare/{file_stem}.rs")]);
+        // NOTE: don't write text to json file, since compare.rs write it in a different way
+        v_json.push(serde_json::from_str(&text).unwrap());
+    }
+
+    assert_unique_hash(&proofs, &v_json);
+    Ok(())
 }
 
 #[test]
