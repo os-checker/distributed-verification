8fd726c295f57112232aa50db33a6fb102f63316
zjp <jiping_zhou@foxmail.com>
2025-04-18 12:21:17 +0800

    chore: remove IndexMap::<DefId, Vec<Instance>> debugs

--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -76,25 +76,6 @@ impl Function {
         let mut callees = IndexSet::new();
         callgraph.recursive_callees(item, &mut callees);
 
-        {
-            // https://github.com/os-checker/distributed-verification/issues/42
-            let mut map = IndexMap::<DefId, Vec<Instance>>::new();
-            for &callee in callees.iter() {
-                map.entry(callee.def.def_id())
-                    .and_modify(|v| v.push(callee))
-                    .or_insert_with(|| vec![callee]);
-            }
-            for (idx, (defid, v)) in map.iter().enumerate() {
-                if defid.name() == "verify::f::kani_register_contract" {
-                    dbg!(v);
-                }
-                let v: Vec<_> = v.iter().map(|i| i.name()).collect();
-                let len = v.len();
-                println!("[{idx} -> {len}] [{defid:?}] {v:?}");
-            }
-            println!("\n\n\n");
-        }
-
         // Multiple instances may share the same defid (or rather SourceCode), so deduplicate them.
         let mut callees: Vec<_> = callees.into_iter().collect();
         callees.sort_by(cache::cmp_callees);
