2aaf703063e872628f67c232081f4858edef1f15
zjp <jiping_zhou@foxmail.com>
2025-04-18 22:42:41 +0800

    logger: without time, target, and anything other than span and content

--- src/logger.rs
+++ src/logger.rs
@@ -1,11 +1,17 @@
 use tracing_subscriber::{EnvFilter, fmt, prelude::*, registry};
 
 pub fn init() {
-    let fmt_layer = fmt::layer();
+    // let fmt_layer = fmt::layer();
+    let fmt_layer = fmt::layer()
+        .with_line_number(false)
+        .with_level(false)
+        .without_time()
+        .with_file(false)
+        .with_target(false);
     let env_layer = EnvFilter::from_default_env();
     let error_layer = tracing_error::ErrorLayer::default();
 
-    if let Err(err) = registry().with(fmt_layer).with(env_layer).with(error_layer).try_init() {
+    if let Err(err) = registry().with(env_layer).with(error_layer).with(fmt_layer).try_init() {
         eprintln!("Logger already init: {err}");
     };
 
