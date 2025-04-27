6601da70d8c55de7b805db49947fd10d7437c7d1
zjp <jiping_zhou@foxmail.com>
2025-04-27 22:24:48 +0800

    ci: replace reusable workflow by install-kani.sh

---
+++ .github/install-kani.sh
@@ -0,0 +1,14 @@
+COMMIT=5cf80a84afdeceec0d85e790a5cc585ff2e513ed
+
+# Install kani
+pushd kani
+./scripts/setup/ubuntu/install_deps.sh
+cargo build-dev
+echo PATH=$(pwd)/scripts:$PATH >>$GITHUB_ENV
+kani --version
+
+popd
+
+pushd tests/dummy-crate
+git clone https://github.com/model-checking/verify-rust-std.git --recursive
+cd verify-rust-std && git checkout $COMMIT
