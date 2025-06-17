13b9f183dbdad1a2702d383440a432ab5581a748
zjp <jiping_zhou@foxmail.com>
2025-06-17 08:22:43 +0000

    ci: adjust to verify-rust-std submodule folder

--- .github/install-kani.sh
+++ .github/install-kani.sh
@@ -4,14 +4,12 @@ set -ex
 # distributed-verification pins another.
 # So this means for verify-rust-std jobs,
 # distributed-verification may be broken to compile.
-COMMIT_VERIFY_RUST_STD=0840b22
-# COMMIT_KANI=d6853436382d876e574fb8a3fdef5b798a6e7d0d
+git submodule update kani --init
 
 # Install kani
 pushd kani
-# git pull origin main --rebase
-# git log -1
-# git checkout $COMMIT_KANI
+git submodule update charon --init
+
 ./scripts/setup/ubuntu/install_deps.sh
 cargo build-dev
 
@@ -20,7 +18,3 @@ echo PATH=$PATH >>$GITHUB_ENV
 kani --version
 
 popd
-
-pushd tests/dummy-crate
-git clone https://github.com/model-checking/verify-rust-std.git --recursive
-cd verify-rust-std && git checkout $COMMIT_VERIFY_RUST_STD
