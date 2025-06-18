e90f2e9b93f40c548442d475ea19c58c615b59af
zjp <jiping_zhou@foxmail.com>
2025-06-18 15:58:26 +0000

    chore(verify_rust_std): add ENV.CARGO_ENCODED_RUSTFLAGS

--- src/bin/verify_rust_std/env.rs
+++ src/bin/verify_rust_std/env.rs
@@ -1,4 +1,5 @@
 use crate::Result;
+use eyre::Context;
 use std::{
     env::var,
     path::{Path, PathBuf},
@@ -13,10 +14,12 @@ pub struct EnvVar {
     pub VERIFY_RUST_STD: String,
     /// Path to verify-rust-std/library
     pub VERIFY_RUST_STD_LIBRARY: PathBuf,
-    /// Path to kani folder where bin, lib, no_core, and playback are sub directories
-    pub KANI_DIR: PathBuf,
+    // Path to kani folder where bin, lib, no_core, and playback are sub directories
+    // pub KANI_DIR: PathBuf,
     /// Path to folder where artifacts like core.json and rustflags.json are put.
     pub OUTPUT_DIR: PathBuf,
+    /// RUSTFLAGS including kani lib path and more, separated by 0x1f
+    pub CARGO_ENCODED_RUSTFLAGS: String,
 }
 
 impl EnvVar {
@@ -51,8 +54,9 @@ pub static ENV: LazyLock<EnvVar> = LazyLock::new(|| EnvVar {
     DISTRIBUTED_VERIFICATION: var_or_string("DISTRIBUTED_VERIFICATION", "distributed-verification"),
     VERIFY_RUST_STD: var_or_string("VERIFY_RUST_STD", "verify_rust_std"),
     VERIFY_RUST_STD_LIBRARY: var_to_path("VERIFY_RUST_STD_LIBRARY"),
-    KANI_DIR: var_to_path("KANI_DIR"),
+    // KANI_DIR: var_to_path("KANI_DIR"),
     OUTPUT_DIR: var_to_path("OUTPUT_DIR"),
+    CARGO_ENCODED_RUSTFLAGS: cargo_encoded_rustflags().unwrap(),
 });
 
 const WRAPPER: &str = "WRAPPER";
@@ -67,4 +71,58 @@ pub fn set_wrapper() -> (&'static str, &'static str) {
 
 pub fn set_rustc_wrapper() -> (&'static str, &'static str) {
     ("RUSTC", &ENV.VERIFY_RUST_STD)
+}
+
+const KANI_ARGS: &[&str] = &[
+    "-C",
+    "overflow-checks=on",
+    "-Z",
+    "unstable-options",
+    "-Z",
+    "trim-diagnostic-paths=no",
+    "-Z",
+    "human_readable_cgu_names",
+    "-Z",
+    "always-encode-mir",
+    "--cfg=kani",
+    "-Z",
+    "crate-attr=feature(register_tool)",
+    "-Z",
+    "crate-attr=register_tool(kanitool)",
+    // "-L",
+    // "/home/zjp/rust/distributed-verification/kani/target/kani/no_core/lib",
+    // "--extern",
+    // "kani_core",
+    "-C",
+    "panic=abort",
+    "-C",
+    "symbol-mangling-version=v0",
+    "-Z",
+    "panic_abort_tests=yes",
+    "-Z",
+    "mir-enable-passes=-RemoveStorageMarkers",
+    "--check-cfg=cfg(kani)",
+];
+
+fn kani_lib_path(kani_dir: &Path) -> Result<PathBuf> {
+    // inject kani_core dependency to recognize kani module in core
+    // -Lpath must be an absolute path
+    let kani_lib = kani_dir.join("no_core").join("lib");
+    kani_lib.canonicalize().with_context(|| format!("{kani_lib:?} can't be canonicalized"))
+}
+
+fn rustc_flags(kani_lib: &Path) -> Result<Vec<String>> {
+    let kani_core = ["-L", kani_lib.to_str().unwrap(), "--extern=kani_core"];
+    Ok(KANI_ARGS.iter().copied().chain(kani_core).map(|arg| arg.to_owned()).collect())
+}
+
+fn cargo_encoded_rustflags() -> Result<String> {
+    let kani_dir = &var_to_path("KANI_DIR");
+    let kani_lib = kani_lib_path(kani_dir)?;
+    Ok(rustc_flags(&kani_lib)?.join("\u{1f}"))
+}
+
+#[test]
+fn test_rustc_flags() {
+    dbg!(cargo_encoded_rustflags().unwrap());
 }
--- src/bin/verify_rust_std/main.rs
+++ src/bin/verify_rust_std/main.rs
@@ -16,7 +16,6 @@ use crate::env::set_wrapper;
 
 fn main() -> Result<()> {
     let mut args = std::env::args().collect::<Vec<_>>();
-    rustc_flags();
 
     if args.len() == 2 && args[1].as_str() == "-vV" {
         // cargo invokes `rustc -vV` first
@@ -52,12 +51,10 @@ fn main() -> Result<()> {
 
 fn run(cmd: &str, args: &[String], vars: &[(&str, &str)]) -> Result<()> {
     let library = &*ENV.VERIFY_RUST_STD_LIBRARY;
-    // CARGO_ENCODED_RUSTFLAGS takes a string that separte arguments by 0x1f
-    let rustc_flags = rustc_flags();
+    let rustflags = &*ENV.CARGO_ENCODED_RUSTFLAGS;
 
-    let _span = debug_span!("run", cmd, ?library, ?args, ?vars, ?rustc_flags).entered();
+    let _span = debug_span!("run", cmd, ?library, ?args, ?vars, rustflags).entered();
 
-    let rustflags = rustc_flags.join("\u{1f}");
     let status = Command::new(cmd)
         .args(args)
         .env("__CARGO_TESTS_ONLY_SRC_ROOT", library)
@@ -71,54 +68,6 @@ fn run(cmd: &str, args: &[String], vars: &[(&str, &str)]) -> Result<()> {
         .with_context(|| "Failed to wait a cmd process")?;
     ensure!(status.success(), "Process aborts.");
     Ok(())
-}
-
-const KANI_ARGS: &[&str] = &[
-    "-C",
-    "overflow-checks=on",
-    "-Z",
-    "unstable-options",
-    "-Z",
-    "trim-diagnostic-paths=no",
-    "-Z",
-    "human_readable_cgu_names",
-    "-Z",
-    "always-encode-mir",
-    "--cfg=kani",
-    "-Z",
-    "crate-attr=feature(register_tool)",
-    "-Z",
-    "crate-attr=register_tool(kanitool)",
-    // "-L",
-    // "/home/zjp/rust/distributed-verification/kani/target/kani/no_core/lib",
-    // "--extern",
-    // "kani_core",
-    "-C",
-    "panic=abort",
-    "-C",
-    "symbol-mangling-version=v0",
-    "-Z",
-    "panic_abort_tests=yes",
-    "-Z",
-    "mir-enable-passes=-RemoveStorageMarkers",
-    "--check-cfg=cfg(kani)",
-];
-
-fn rustc_flags() -> Vec<String> {
-    // inject kani_core dependency to recognize kani module in core
-    let kani_dir = &*ENV.KANI_DIR;
-    // -Lpath must be an absolute path
-    let kani_lib = kani_dir.join("no_core").join("lib");
-    let kani_lib = kani_lib.canonicalize().unwrap_or_else(|err| panic!("{kani_lib:?}: {err}"));
-    assert!(std::fs::exists(&kani_lib).unwrap());
-    let kani_core = ["-L", kani_lib.to_str().unwrap(), "--extern=kani_core"];
-
-    KANI_ARGS.iter().copied().chain(kani_core).map(|arg| arg.to_owned()).collect()
-}
-
-#[test]
-fn test_rustc_flags() {
-    dbg!(rustc_flags());
 }
 
 fn build_core(args: Vec<String>) -> Result<()> {
