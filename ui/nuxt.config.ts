263d6e732a2317677c1c714640998b5789b4b231
zjp <jiping_zhou@foxmail.com>
2025-08-07 14:55:58 +0000

    ui: init nuxt & primevue template

---
+++ ui/nuxt.config.ts
@@ -0,0 +1,20 @@
+import Aura from '@primeuix/themes/aura';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  typescript: {
+    typeCheck: true
+  },
+  modules: [
+    '@primevue/nuxt-module'
+  ],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura
+      }
+    }
+  }
+})
