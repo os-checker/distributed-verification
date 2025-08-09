22cf9ac0509309a718cc102c95c1715b71bfcb27
zjp <jiping_zhou@foxmail.com>
2025-08-09 09:02:37 +0000

    ui: add theme toggle button

--- ui/app/layouts/default.vue
+++ ui/app/layouts/default.vue
@@ -1,20 +1,32 @@
 <script setup lang="ts">
 
 const CLASS_DARK = "my-app-dark";
+const KEY = "theme";
+const DARK = "dark";
+
+const dark = ref(false);
+
 function toggleTheme() {
   const isDark = document.documentElement.classList.toggle(CLASS_DARK);
-  localStorage.setItem("theme", isDark ? "dark" : "light");
+  dark.value = isDark;
+  localStorage.setItem(KEY, isDark ? DARK : "light");
 }
 
-if (localStorage.getItem("theme") === "dark") {
+if (localStorage.getItem(KEY) === DARK) {
   document.documentElement.classList.add(CLASS_DARK);
+  dark.value = true;
 }
 
 </script>
 
 <template>
   <div>
-    <Button icon="pi pi-refresh" @click="toggleTheme" rounded raised />
+    <div class="flex justify-between p-2">
+      <div></div>
+      <div>
+        <Button :icon='dark ? "pi pi-sun" : "pi pi-moon"' @click="toggleTheme" severity="contrast" raised />
+      </div>
+    </div>
 
     <slot />
   </div>
