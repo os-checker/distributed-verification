c4084cfa92c8d6d8575c65fe82210add6018eacf
zjp <jiping_zhou@foxmail.com>
2025-08-19 09:01:02 +0000

    split: parallel JSON file generation, time reduced from 9mins to 25s

--- assets/split.sh
+++ assets/split.sh
@@ -10,13 +10,26 @@ base=$3
 
 echo "db=$db, sql=$sql, base=$base"
 
-PREFIX=/home/runner/work/verify-rust-std/verify-rust-std/library/
+# Let parallel use bash.
+export PARALLEL_SHELL=$(type -p bash)
 
-sqlite3 -batch "$db" -separator $'\t' <"$sql" |
+run() {
+  PREFIX=/home/runner/work/verify-rust-std/verify-rust-std/library/
+
+  # Read all arguments and JSON from stdin through parallel pipe.
   while IFS=$'\t' read -r filename hash json; do
-    json_folder="$base/${filename#"$PREFIX"}"
+    # $base is unavaible in parallel run, so pass it as env var.
+    json_folder="$BASE/${filename#"$PREFIX"}"
     json_file="$json_folder/$hash.json"
     echo "$json_file"
     mkdir -p "$json_folder"
     jq 'del(.[] | select(. == []))' <<<"$json" >"$json_file"
   done
+}
+
+# Let parallel call the function.
+export -f run
+
+sqlite3 -batch "$db" -separator $'\t' <"$sql" |
+  parallel --colsep '\t' --pipe --progress --tagstring '[Job {#}]' \
+    "BASE=$base run"
