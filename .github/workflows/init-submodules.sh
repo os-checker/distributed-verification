b97118e85ad4d0f6b19a0ebf515d09937d7eb336
zjp <jiping_zhou@foxmail.com>
2025-10-29 13:28:23 +0000

    ci(init-submodules): mnt more target folders

--- .github/workflows/init-submodules.sh
+++ .github/workflows/init-submodules.sh
@@ -15,20 +15,23 @@ fi
 
 echo "Mount /mnt (/dev/sda1) to verify-rust-std because the available space is $((avail_k / 1024 / 1024))G"
 
-TARGET_DIR=$PWD/verify-rust-std/target
+declare -A map_mnt
+
+map_mnt["$PWD/verify-rust-std/target"]="/mnt/verify-rust-std/target"
+map_mnt["$PWD/verify-rust-std/library/target"]="/mnt/verify-rust-std/library/target"
+map_mnt["$PWD/kani/target"]="/mnt/kani/target"
 
 # We can't store data on /dev/sda1 by mounting verify-rust-std directly.
 # Kani stores verify-rust-std/target which takes a lot space, so we mount that folder.
 # Note: there os a verify-rust-std/library/target folder as well which is not mounted to the sda1 dist.
-mkdir "$TARGET_DIR"
 
-sudo mkdir -p /mnt/my
-sudo chown runner:runner /mnt/my
-sudo chmod 755 /mnt/my
-
-sudo mount --bind /mnt/my "$TARGET_DIR"
-
-ls -alh "$TARGET_DIR"
-ls -alh /mnt/my
+for key in "${!map_mnt[@]}"; do
+  val=${map_mnt[$key]}
+  mkdir -p "$key"
+  sudo mkdir -p "$val"
+  sudo chown runner:runner "$val"
+  sudo chmod 755 "$val"
+  sudo mount --bind "$val" "$key"
+done
 
 df -alh
