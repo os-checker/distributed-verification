fee9bbb8fdf93c23a5946bed306b530b3ba5183d
zjp <jiping_zhou@foxmail.com>
2025-11-01 10:21:57 +0000

    ci: partition consecutive harnesses

--- .github/actions/init/partition.sh
+++ .github/actions/init/partition.sh
@@ -18,11 +18,21 @@ OUT_DIR=partition
 mkdir $OUT_DIR -p
 
 # Split the array to $N batches, with length as much close as possible.
-# The order is not guaranteed: consecutive harnesses are split into
-# differenct groups. Say [1,2,3,4] is partitioned into `[1,3]` and
-# `[2,4]` with N=2.
+# Consecutive harnesses are split into the same group. Say [1,2,3,4] is
+# partitioned into `[1,2]` and `[3,4]` with N=2.
 jq -c --argjson N $N '
-. as $in | [range(length)] | group_by(. % $N) | map(map($in[.])) | .[]
+  . as $in
+  | length as $L
+  | ($L / $N | floor) as $base_size
+  | ($L % $N) as $remainder
+  | ([range($remainder) | $base_size + 1] + [range($N - $remainder) | $base_size]) as $sizes
+  | reduce $sizes[] as $size (
+      {slices: [], offset: 0};
+      .slices += [$in[.offset : .offset + $size]] |
+      .offset += $size
+    )
+  | .slices
+  | .[]
 ' kani-list_arr.json | while read -r line; do
   echo "$line" >"$OUT_DIR/${i}.json"
   i=$((i + 1))
